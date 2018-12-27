78603d98ac7d0c9c451cbe7b76a47b8bab3d7848
szwchao <szwchao@gmail.com>
2018-12-27 14:47:22 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 获取平台类型，mac还是linux平台
 function get_platform_type()
 {
     echo $(uname)
@@ -10,13 +9,12 @@ function clone_repo()
 {
     rm -rf ~/.vim_bak
     mv ~/.vim ~/.vim_bak
-    git clone https://github.com/szwchao/vim ~/.vim
+    git clone --depth=1 https://github.com/szwchao/vim ~/.vim
     cd ~/.vim
     git fetch origin test:test
     git checkout test
 }
 
-# 获取linux平台类型，ubuntu还是centos
 function get_linux_platform_type()
 {
     if which apt-get > /dev/null ; then
@@ -30,37 +28,29 @@ function get_linux_platform_type()
     fi
 }
 
-# 安装mac平台必要软件
 function install_prepare_software_on_mac()
 {
     brew install vim gcc cmake ctags-exuberant curl ack
 }
 
-# 安装ubuntu发行版必要软件
 function install_prepare_software_on_ubuntu()
 {
     sudo apt-get install -y ctags build-essential cmake python-dev python3-dev fontconfig curl libfile-next-perl ack-grep
     sudo apt-get install -y vim vim-gtk
 }
 
-# 拷贝文件
 function copy_files()
 {
     rm -rf ~/.vimrc
-    ln -s ~/.vim/.vimrc ~/.vim
-
-    rm -rf ~/.vimrc.local
-    ln -s ~/.vim/.vimrc.plugins ~/.vim
+    ln -s ~/.vim/.vimrc ~/.vimrc
 }
 
-# 安装mac平台字体
 function install_fonts_on_mac()
 {
     rm -rf ~/Library/Fonts/Droid\ Sans\ Mono\ Nerd\ Font\ Complete.otf
     cp ./fonts/Droid\ Sans\ Mono\ Nerd\ Font\ Complete.otf ~/Library/Fonts
 }
 
-# 安装linux平台字体
 function install_fonts_on_linux()
 {
     mkdir ~/.fonts
@@ -69,19 +59,16 @@ function install_fonts_on_linux()
     fc-cache -vf ~/.fonts
 }
 
-# 下载插件管理软件vim-plug
 function download_vim_plug()
 {
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
-# 安装vim插件
 function install_vim_plugin()
 {
     vim -c "PlugInstall" -c "q" -c "q"
 }
 
-# 打印logo
 function print_logo()
 {
     color="$(tput setaf 6)"
@@ -101,7 +88,6 @@ function print_logo()
     printf "${normal}"
 }
 
-# 在mac平台安装vimplus
 function install_vimplus_on_mac()
 {
     clone_repo
@@ -123,15 +109,12 @@ function begin_install_vimplus()
     print_logo
 }
 
-# 在ubuntu发行版安装vimplus
 function install_vimplus_on_ubuntu()
 {
     install_prepare_software_on_ubuntu
-    download_font
     begin_install_vimplus
 }
 
-# 在linux平台安装vimplus
 function install_vimplus_on_linux()
 {
     type=`get_linux_platform_type`
@@ -144,7 +127,6 @@ function install_vimplus_on_linux()
     fi
 }
 
-# main函数
 function main()
 {
     type=`get_platform_type`
@@ -159,5 +141,4 @@ function main()
     fi
 }
 
-# 调用main函数
 main
